b002cc84935b9ffb8ed9dded358e55207eb13c6b
dosmatto <dosmatto@gmail.com>
2026-06-09 13:13:09 -0300

    Compacta cards de lancamento

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { USE_FIREBASE, firebaseConfig } from "./firebase-config.js";
   const ADMIN_TOKEN = "ADMIN-TESTE-2026";
   const MASTER_ACCOUNT_ID = "master";
   const FIREBASE_DOC_PATH = ["appState", "main"];
-  const APP_VERSION = "V.2.9";
+  const APP_VERSION = "V.2.10";
   const EXPIRY_WARNING_DAYS = 30;
 
   const categories = [
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "estoque-fazendas-v2-9";
+const CACHE_NAME = "estoque-fazendas-v2-10";
 const ASSETS = [
   "./",
   "./index.html",
